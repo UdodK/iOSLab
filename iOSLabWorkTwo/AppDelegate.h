ede4030ca6ae6059dcc997f6b24fdf545c9907f6
UdodK <buka270@gmail.com>
2014-08-19 13:20:47 +0400

    Initial Commit

---
+++ iOSLabWorkTwo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSLabWorkTwo
+//
+//  Created by Macbook Air on 19.08.14.
+//  Copyright (c) 2014 Macbook Air. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSLabWorkTwo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSLabWorkTwo
+//
+//  Created by Macbook Air on 19.08.14.
+//  Copyright (c) 2014 Macbook Air. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
